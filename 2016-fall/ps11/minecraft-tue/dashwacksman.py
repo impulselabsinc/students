8365760127c88e460a7e995811c181373f86209e
impulselabsinc <nikhil@impulselabs.io>
2016-09-23 11:54:19 -0400

    tnt-dashwacksman

---
+++ 2016-fall/ps11/minecraft-tue/dashwacksman.py
@@ -0,0 +1,9 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+tnt=bLOCK TNT EXPLODING
+MC=MINECRAFT.MINECRAFT.CREATE()
+pos=mc.player.getTilePos()
+x=posx+1
+y=pos.y
+2.2+1
+mc.set Block(x,y,2,tnt)
