c273f8bc4b4aa8856a230d43421e0fa78f2c6bba
impulselabsinc <nikhil@impulselabs.io>
2016-09-23 11:58:39 -0400

    tnt-esayah

---
+++ 2016-fall/ps11/minecraft-tue/esayah.py
@@ -0,0 +1,9 @@
+import mcpi.minecraft as minecraft
+import mcpi.minecraft as block
+tnt = block.TNT_EXPLODING
+mc=minecraft.Minecraft.create()
+pos=mc.player.getTilePos()
+x=pos.x+1
+y=pos.y
+z=pos.z+1
+mc.setBlock(x,y,z,tnt)
