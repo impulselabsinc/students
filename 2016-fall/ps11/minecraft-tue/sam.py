ebfc9a62c8c12f794ddb4ab0de03e3eee3a1d69d
impulselabsinc <nikhil@impulselabs.io>
2016-09-29 21:31:12 -0400

    sam

--- 2016-fall/ps11/minecraft-tue/sam.py
+++ 2016-fall/ps11/minecraft-tue/sam.py
@@ -1,1 +1,17 @@
-import mcpi.minecraft as minecraft import mcpi.block mc=
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+mc=minecraft.Minecraft.create()
+
+
+tnt=block.TNT_EXPLODING
+pos=mc.player.getTilepos()
+x=pos.x+1
+y=pos.y
+z=pos.z+1
+mc.setbiock(x,y,z,tnt)
+            
+
+
+
+
+
