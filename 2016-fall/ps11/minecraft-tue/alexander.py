14cacadbe579041a2c7ade26b4b02b3b103d98e8
impulselabsinc <nikhil@impulselabs.io>
2016-09-29 21:15:31 -0400

    alexander

--- 2016-fall/ps11/minecraft-tue/alexander.py
+++ 2016-fall/ps11/minecraft-tue/alexander.py
@@ -1,1 +1,13 @@
-import mcpi.minecraft as minecraft import tnt=block,TNT-EXPlODINGmc=
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+
+
+mc =minecraft.MINECRAFT.create()
+tnt =block.TNT_EXPLODING
+POS =MC.player.gettilepos{}
+x =pos.x +1
+y =pos.y
+z =pos.z +1
+
+
+mc.setBlOCK{x, y, z, tnt}
