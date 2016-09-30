f9e7aae6145e8489d05029215c8525e48ab66207
impulselabsinc <nikhil@impulselabs.io>
2016-09-29 21:06:10 -0400

    azelia

--- 2016-fall/ps11/minecraft-tue/azelia-mccall.py
+++ 2016-fall/ps11/minecraft-tue/azelia-mccall.py
@@ -1,8 +1,11 @@
-importmcpi.mincraftasmincraft
-importmcpi.block
-mc=mincraft.mincraft.create()
-pos=mc.player.gettilepos()
-x=pos.x+1
-y=pos.y
-z=z+1
+import mcpi.mincraft as mincraft
+import mcpi.block as block
+
+mc=mincraft.Mincraft.create()
+tnt = block.TNT_EXPLODING
+pos = mc.player.getTilepos()
+x = pos.x + 1
+y = pos.y
+z = z + 1
+
 mc.setblock(x,y,z,tnt)
