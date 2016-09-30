f41d9dca840d835280ffabbddf428f7b8aa96ad2
impulselabsinc <nikhil@impulselabs.io>
2016-09-29 21:28:33 -0400

    max

---
+++ 2016-fall/ps11/minecraft-tue/max.py
@@ -0,0 +1,13 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+
+mc=minecraft.create()
+tnt=block.TNT_EXPLODING
+Pos=mc.player.getTilePos()
+x=pos.x + 1
+y=pos.y
+z=pos.z + 1
+
+
+
+
