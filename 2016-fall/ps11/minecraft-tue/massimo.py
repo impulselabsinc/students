17024957ac1cadbc448a5c83209bc738c8e82ccc
impulselabsinc <nikhil@impulselabs.io>
2016-09-29 21:19:35 -0400

    massimo

--- 2016-fall/ps11/minecraft-tue/massimo.py
+++ 2016-fall/ps11/minecraft-tue/massimo.py
@@ -1,7 +1,8 @@
-import mepi.minecraft as minecraft
-import mepi.block as block
-tnt=block.TNT_EXPLODING
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+
 mc=minecraft.Minecraft.create()
+tnt=block.TNT_EXPLODING
 pos=mc.player.getTilePos()
 x=pos.x+1
 y=pos.y
