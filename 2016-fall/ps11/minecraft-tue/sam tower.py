7d31b99af49c103687a26834050ae3da31b7bf89
impulselabsinc <nikhil@impulselabs.io>
2016-10-23 14:31:49 -0400

    lucy olufemi sam

---
+++ 2016-fall/ps11/minecraft-tue/sam tower.py
@@ -0,0 +1,19 @@
+import mcpi.minecraft as minecraft
+
+import mcpi.block as block
+
+
+
+
+mc = minecraft.Minecraft.create()
+myblock = block.GOLD_ORE
+pos = mc.player.getTilePos()
+x = pos.x + 1
+y = pos.y
+z =pos.z + 1
+
+
+
+
+
+mc.setBlocks(x, y, z, x+11, y+11, z+11, myblock)
