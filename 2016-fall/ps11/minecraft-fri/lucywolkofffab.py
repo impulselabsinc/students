52816ae7a6e900c6195a1d94cdbec9d0d93cc713
impulselabsinc <nikhil@impulselabs.io>
2016-10-23 14:03:32 -0400

    naasir lucy azelia

---
+++ 2016-fall/ps11/minecraft-fri/lucywolkofffab.py
@@ -0,0 +1,10 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+
+mc = minecraft.Minecraft.create()
+myBlock = block.COBWEB
+
+
+myPosition = mc.player.getTilePos()
+
+mc.setBlock(myPosition.x+1, myPosition.y+1, myPosition.z, myBlock)
