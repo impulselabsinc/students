1782ccd0cb28011e951d99a7cf2bbd8f57a54a5b
impulselabsinc <nikhil@impulselabs.io>
2016-11-11 16:43:18 -0500

    stuff

--- 2016-fall/ps11/minecraft-fri/azaraputbluck.py
+++ 2016-fall/ps11/minecraft-fri/azaraputbluck.py
@@ -5,4 +5,4 @@ mc = minecraft.Minecraft.create()
 myBlock = block.TNT
 
 myPosition = mc.player.getTilePos ()
-mc.setBlock(myPosition.x+1, myPosition.y+1, myPosition.z, myBlock)
+mc.setBlock(myPosition.x+0, myPosition.y+2, myPosition.z, myBlock)
