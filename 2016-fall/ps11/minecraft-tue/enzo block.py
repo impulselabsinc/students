3446b7adf19c1ec86c8981a9d5291f05126600b5
impulselabsinc <nikhil@impulselabs.io>
2016-11-12 11:42:30 -0500

    stuff

--- 2016-fall/ps11/minecraft-tue/enzo block.py
+++ 2016-fall/ps11/minecraft-tue/enzo block.py
@@ -2,8 +2,8 @@ import mcpi.minecraft as minecraft
 import mcpi.block as block
 
 mc = minecraft.Minecraft.create()
-myBlock = block.TNT
+myBlock = block.WOOL_LIGHTBLUE
+
 
 myPosition = mc.player.getTilePos()
 mc.setBlock(myPosition.x+0, myPosition.y-1, myPosition.z, myBlock)
-            
