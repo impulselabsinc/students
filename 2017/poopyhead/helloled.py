c293448e5152b93c1e07e54ac8b0d87ff7ff99fc
impulselabsinc <nikhil@impulselabs.io>
2017-05-07 23:05:27 -0400

    led

--- 2017/poopyhead/helloled.py
+++ 2017/poopyhead/helloled.py
@@ -1,11 +1,17 @@
 import RPi.GPIO as GPIO
-LED_PIN = 18
+import time
+
+LED_PIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
 
 
 try:
-    GPIO.output(LED_PIN, False)
+    for x in range(0,10):
+        print x
+        GPIO.output(LED_PIN, True)
+        time.sleep(0.2)
+        GPIO.output(LED_PIN, False)
 
 finally:
     GPIO.cleanup()
